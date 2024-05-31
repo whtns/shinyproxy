d657639f8e075cf4b898b08f56a01880eff9889f
whtns <kevin.stachelek@gmail.com>
2024-05-31 13:15:35 -0700

    user dockerApp function for bigwig plotting

--- dockerapp/app.R
+++ dockerapp/app.R
@@ -16,5 +16,5 @@ library(shinyFiles)
 # seu <- readRDS("/dataVolume/storage/single_cell_projects/shinyproxy/dockerdata/Final_dataset_Clean_public_061223.rds")
 seu <- readRDS("/root/dockerdata/Final_dataset_Clean_public_061223.rds")
 
-# dockerSeuratApp(seu, bigwig_db="/root/dockerdata/bw-files.db")
-minimalSeuratApp(seu)
+dockerSeuratApp(seu, bigwig_db="/root/dockerdata/bw-files.db")
+# minimalSeuratApp(seu)
